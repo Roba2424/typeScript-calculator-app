0f2f7f658eb5203701d454125fc2a68c346a4599
Robert Davidyan <robert.davidyan@Roberts-MacBook-Pro.local>
2024-11-20 18:11:53 +0400

    feat:add functionality in calculator component

--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -1,13 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
 import { buttons } from "../../utils/constants";
+import { RootState } from "../../state-managment/store/store";
 import "./style.css";
+import {
+  clear,
+  inputDigit,
+  performOperation,
+} from "../../state-managment/slices/calculatorSlice";
 
 const Calculator = () => {
+  const dispatch = useDispatch();
+  const displayValue = useSelector(
+    (state: RootState) => state.calculator.displayValue
+  );
+
+  const handleOperation = (value: string) => {
+    if (value === "C") dispatch(clear());
+    else if (["+", "-", "*", "/"].includes(value)) dispatch(inputDigit(value));
+    else if (value === "=") dispatch(performOperation(value));
+    else dispatch(inputDigit(value));
+  };
+
   return (
     <div className="calculator">
-      <div className="display"></div>
+      <div className="display">{displayValue}</div>
       <div className="buttons">
         {buttons.map((btn) => (
-          <button key={btn}>{btn}</button>
+          <button key={btn} onClick={() => handleOperation(btn)}>
+            {btn}
+          </button>
         ))}
       </div>
     </div>
